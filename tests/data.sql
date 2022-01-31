0d61bbabc5a659a6124180b6537f0d64d8c092bc
Moi <jeremy.cao.pro@gmail.com>
2022-01-31 22:34:30 +0100

    All the tests are working

--- tests/data.sql
+++ tests/data.sql
@@ -1,3 +1,3 @@
-INSERT INTO pdf_content (metadata, pdf_name, uploaded, author, boby, title)
+INSERT INTO pdf_content (metadata, pdf_name, uploaded, author, body, title)
 VALUES
   ('test' || x'0a' || 'metadata', 'test name', '2018-01-01 00:00:00', 'test author',  'test' || x'0a' || 'body', 'test title');
